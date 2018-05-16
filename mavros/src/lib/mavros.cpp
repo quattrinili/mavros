227b21d0c9398f2be53d88abbcdd42c3204c125f
Alberto Quattrini Li <albertoq@cse.sc.edu>
2018-05-16 16:30:36 -0400

    Included a modification for publishing only sensor data for a specific vehicle according to the tgt_sys_id.

--- mavros/src/lib/mavros.cpp
+++ mavros/src/lib/mavros.cpp
@@ -214,7 +214,7 @@ void MavRos::mavlink_sub_cb(const mavros_msgs::Mavlink::ConstPtr &rmsg)
 void MavRos::plugin_route_cb(const mavlink_message_t *mmsg, const Framing framing)
 {
 	auto it = plugin_subscriptions.find(mmsg->msgid);
-	if (it == plugin_subscriptions.end())
+	if (it == plugin_subscriptions.end() || mmsg->sysid != mav_uas.get_tgt_system())
 		return;
 
 	for (auto &info : it->second)
